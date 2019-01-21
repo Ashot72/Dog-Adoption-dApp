ffd4341741f62d0d242e9c3a9a3d202360b6c0f5
Ashot Abrahamyan <ashota@gmail.com>
2019-01-21 16:43:39 +0400

    removing code from default project

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,21 +13,6 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [
-        { icon: "apps", title: "Welcome", to: "/" },
-        { icon: "bubble_chart", title: "Inspire", to: "/inspire" }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: "Vuetify.js"
-    };
-  },
   components: {
     Navbar,
     Footer
